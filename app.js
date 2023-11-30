545ec2b333a18d0e4a9472eb7a8782a5cd9127bf
Allison <farrella95@gmail.com>
2023-11-30 17:57:13 -0500

    begin function

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 let slide = document.querySelector(".switch");
+let monthly = document.querySelector("#monthly");
+let annual = document.querySelector("#annual");
 
-slide.addEventListener("click", (e) => {
-  //if not active, show annual
-  //if active, show monthly
-});
+function showPrice(slide) {
+  if (slide.checked) {
+    // annual.classList.add("hidden");
+    // monthly.classList.remove("hidden");
+    console.log("monthly");
+  } else {
+    // monthly.classList.add("hidden");
+    // annual.classList.remove("hidden");
+    console.log("annual");
+  }
+}
