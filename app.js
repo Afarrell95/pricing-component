9b77ad44cfe0afe42820b836883baa2d46b01bf5
Allison <farrella95@gmail.com>
2023-12-07 13:28:49 -0500

    javascript works, just needs styling

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-let slide = document.querySelector(".switch");
-let monthly = document.querySelector("#monthly");
-let annual = document.querySelector("#annual");
+const slide = document.querySelector("input[type='checkbox']");
 
-function showPrice(slide) {
-  if (slide.checked) {
-    // annual.classList.add("hidden");
-    // monthly.classList.remove("hidden");
-    console.log("monthly");
-  } else {
-    // monthly.classList.add("hidden");
-    // annual.classList.remove("hidden");
-    console.log("annual");
-  }
-}
+slide.addEventListener("change", function () {
+  const plans = document.querySelectorAll(".price");
+
+  plans.forEach(function (plan) {
+    if (slide.checked) {
+      if (plan.id.includes("monthly")) {
+        plan.classList.remove("hidden");
+      } else {
+        plan.classList.add("hidden");
+      }
+    } else {
+      if (plan.id.includes("annual")) {
+        plan.classList.remove("hidden");
+      } else {
+        plan.classList.add("hidden");
+      }
+    }
+  });
+});
